87afbea576bdd6b4ed282e1212a23bdbd412f208
Jan Egil Staff Øvretveit <janegilstaffovretveit@Jan-sin-MacBook-Air.local>
2025-04-23 17:06:53 +0200

    Front End Setup with Layout

---
+++ client/src/components/DefaultLayout.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "../resources/default-layout.css";
+
+const DefaultLayout = (props) => {
+  return (
+    <div className="layout">
+      <div className="header d-flex justify-content-between align-items-center">
+        <div>
+            <h1 className="logo">Expense Tracker</h1>
+        </div>
+        <div>
+            <h1 className="username">Username</h1>
+        </div>
+      </div>
+
+      <div className="content">{props.children}</div>
+    </div>
+  );
+};
+
+export default DefaultLayout;
